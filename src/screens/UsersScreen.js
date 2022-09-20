1278ad922698deb35c2dd4aab2c7e1241da57217
alihaiderdev <ah1675179@gmail.com>
2022-09-20 12:19:20 +0500

    script for updating data in strapi

--- src/screens/UsersScreen.js
+++ src/screens/UsersScreen.js
@@ -9,6 +9,30 @@ const UsersScreen = () => {
   // console.log({ isLoading, error, users, total });
   const dispatch = useDispatch();
 
+  // useEffect(() => {
+  //   if (categories?.length > 0) {
+  //     categories?.forEach(async (category) => {
+  //       const res = await fetch(
+  //         `${Api_constant['domain']}${Api_constant['sub_domain']['get_category']}/${category.id}`,
+  //         {
+  //           method: 'PUT',
+  //           headers: {
+  //             Authorization:
+  //               'Bearer 0aad178b2a110c9a23a2d7b41f88a30146e3c6542170664edd442992eaec12dfb8ca7c1e89ff0977a4f236831aea193fb90f0096c0333693fd735045690048591b05abb6939fe08b492418cfc90ab638c0e5b7de191791198f57e699760c06a581b8071680a93d350cf32abf30e5702124ea0cf729d505ff5872c208d9713082',
+  //           },
+  //           body: JSON.stringify({
+  //             data: {
+  //               logo_img: '/images/sitemap/',
+  //             },
+  //           }),
+  //         }
+  //       );
+  //       const data = await res.json();
+  //       console.log({ data });
+  //     });
+  //   }
+  // }, [categories?.length]);
+
   useEffect(() => {
     dispatch(fetchUsers());
   }, []);
