f9102ad35ba23bb207c3014c503a1cdb5246148e
alihaiderdev <ah1675179@gmail.com>
2022-09-29 13:42:16 +0500

    done with order success page

--- src/screens/OrderSuccess.js
+++ src/screens/OrderSuccess.js
@@ -1,14 +1,55 @@
-import React, { useEffect } from "react";
-import { useAuthAndCartContext } from "../context";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { useAuthAndCartContext } from '../context';
+
+const useOrder = (session_id) => {
+  const [order, setOrder] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+  const { user } = useAuthAndCartContext();
+
+  useEffect(() => {
+    if (session_id) {
+      const fetchOrder = async () => {
+        try {
+          setIsLoading(true);
+          const { data } = await axios(`/api/orders/confirm`, {
+            method: 'POST',
+            data: JSON.stringify({ data: { checkout_session: session_id } }),
+          });
+
+          setOrder(data);
+        } catch (error) {
+          setError(error.message);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+      fetchOrder();
+    }
+  }, [session_id]);
+
+  return { order, isLoading, error };
+};
 
 const OrderSuccess = () => {
+  let [searchParams, setSearchParams] = useSearchParams();
   const { clearCart } = useAuthAndCartContext();
+  const { order, isLoading, error } = useOrder(searchParams.get('session_id'));
 
   useEffect(() => {
     clearCart();
+    console.log(searchParams.get('session_id'));
+    console.log({ order, isLoading, error });
   }, []);
 
-  return <div>OrderSuccess</div>;
+  return (
+    <div>
+      Your order has been confirm with the order id: {order.id} and status:{' '}
+      {order.status}
+    </div>
+  );
 };
 
 export default OrderSuccess;
